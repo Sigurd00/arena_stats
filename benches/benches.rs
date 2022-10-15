6b03e36b9556d3c27d82e31fece01ed0f214fa21
jskad <j.skadborg00@gmail.com>
2022-10-15 16:07:38 +0200

    Made benchmark for parser and analyser

--- benches/benches.rs
+++ benches/benches.rs
@@ -1,7 +1,14 @@
-use arena_stats::{self, run};
-use criterion::{criterion_group, criterion_main, Criterion};
+use arena_stats::{analyser, parser};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+
 pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("full program", |b| b.iter(|| run::run()));
+    let games = parser::parse_games(String::from("testdata.csv").into()).unwrap();
+    c.bench_function("parser", |b| {
+        b.iter(|| parser::parse_games(String::from("testdata.csv").into()))
+    });
+    c.bench_with_input(BenchmarkId::new("analyser", "games"), &games, |b, s| {
+        b.iter(|| analyser::start(s))
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
